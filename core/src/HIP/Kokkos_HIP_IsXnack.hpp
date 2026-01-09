45a1d48291c9b87e5b962cf23748725d6a1eb938
Kevin Huck <kevin.huck@amd.com>
2026-01-09 16:32:10 -0600

    Adding support for AMD MI355 and MI350. The offload arch for both is gfx950.
    Signed-off-by: Kevin Huck <Kevin.Huck@amd.com>

--- core/src/HIP/Kokkos_HIP_IsXnack.hpp
+++ core/src/HIP/Kokkos_HIP_IsXnack.hpp
@@ -38,7 +38,8 @@ bool xnack_boot_config_has_hmm_mirror();
 // allocated memory
 constexpr bool gpu_arch_can_access_system_allocations() {
 #if defined(KOKKOS_ARCH_AMD_GFX908) || defined(KOKKOS_ARCH_AMD_GFX90A) || \
-    defined(KOKKOS_ARCH_AMD_GFX942) || defined(KOKKOS_ARCH_AMD_GFX942_APU)
+    defined(KOKKOS_ARCH_AMD_GFX942) || defined(KOKKOS_ARCH_AMD_GFX942_APU) || \
+    defined(KOKKOS_ARCH_AMD_GFX950)
   return true;
 #elif defined(KOKKOS_ARCH_AMD_GFX906) || defined(KOKKOS_ARCH_AMD_GFX1103) || \
     defined(KOKKOS_ARCH_AMD_GFX1100) || defined(KOKKOS_ARCH_AMD_GFX1030) ||  \
